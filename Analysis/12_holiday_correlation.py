f49312b47e284bba3d22095d2004d66b6ae61619
Siobhan <spowell2@stanford.edu>
2021-11-12 10:50:09 -0800

    pvalue on correlation

--- Analysis/12_holiday_correlation.py
+++ Analysis/12_holiday_correlation.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import numpy as np
+from scipy import stats
+
 os.chdir('..')
 df_init = pd.read_csv('Data/Intermediate/panel_data.csv', index_col=0)
 df_recov = pd.read_csv('Data/Intermediate/panel_data_recovery.csv', index_col=0)
@@ -11,6 +14,10 @@ for i in res.index:
     res.loc[i, 'Min'] = min(subset1['Pct Change in Electricity Demand'].min(), subset2['Pct Change in Electricity Demand'].min())
     res.loc[i, 'Holiday Effect'] = subset1['Holiday Effect'].min()
     
-print('Results:')
+print('Results with Pandas:')
 print(res.corr().loc['Holiday Effect'])
+print('------'*10)
+
+print('Scipy:')
+print(stats.pearsonr(res['Min'].values, res['Holiday Effect'].values))
 print('------'*10)
