b9d64b4d0c8546a70f134cde29425a788c16043d
Siobhan <spowell2@stanford.edu>
2021-11-21 22:03:10 -0800

    arial

--- Analysis/13_abstract_figures.py
+++ Analysis/13_abstract_figures.py
@@ -18,6 +18,7 @@ subset = all_data[pd.to_datetime(all_data['Date']).dt.date <= datetime.date(2020
 subset2 = all_data[pd.to_datetime(all_data['Date']).dt.date > datetime.date(2020, 4, 23)].reset_index(drop=True)
 titles = ['Extreme', 'Severe', 'Mild']
 fig, axes = plt.subplots(figsize=(8,5))#, sharey=True)
+period = 'Initial'
 for i in range(3):
     countries = full_cluster_mapping[full_cluster_mapping[period]==i]['Country'].values
     axes.plot(pd.to_datetime(subset['Date']), subset.loc[:, countries].mean(axis=1), color=colors[i], zorder=3, label=titles[i], linewidth=3)
@@ -25,6 +26,7 @@ for i in range(3):
 titles = ['Slow', 'Quick', 'Recovered']
 subset2 = all_data[pd.to_datetime(all_data['Date']).dt.date <= datetime.date(2020, 5, 3)].reset_index(drop=True)
 subset = all_data[pd.to_datetime(all_data['Date']).dt.date > datetime.date(2020, 4, 23)].reset_index(drop=True)
+period = 'Recovery'
 for i in range(3):
     countries = full_cluster_mapping[full_cluster_mapping[period]==i]['Country'].values
     axes.plot(pd.to_datetime(subset['Date']), subset.loc[:, countries].mean(axis=1), color=colors[i+3], zorder=3, label=titles[i], linewidth=3)
@@ -34,14 +36,14 @@ axes.axhline(color='k', alpha=0.3, zorder=1)
 axes.axvline(datetime.date(2020,4,28), linestyle='--', color='k', alpha=0.7, zorder=1)
 axes.xaxis.set_major_locator(mdates.MonthLocator())
 axes.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-axes.set_xlabel('Month', fontsize=16)
-axes.set_ylabel('% Change Electricity Consumption', fontsize=16)
+axes.set_xlabel('Month', fontsize=16, fontname='Arial')
+axes.set_ylabel('% Change Electricity Consumption', fontsize=16, fontname='Arial')
 axes.set_yticks([-25, -20, -15, -10, -5, 0])
-axes.set_yticklabels([-25, -20, -15, -10, -5, 0], fontsize=16)
+axes.set_yticklabels([-25, -20, -15, -10, -5, 0], fontsize=16, fontname='Arial')
 axes.set_ylim([-28, 3])
 axes.set_xlim([datetime.date(2020,1,1), datetime.date(2020, 10, 27)])
-axes.legend(fontsize=16, ncol=2, loc='lower right')
-plt.xticks(fontsize=16)
+axes.legend(fontsize=16, ncol=2, loc='lower right', prop={'family':'Arial', 'size':16})
+plt.xticks(fontsize=16, fontname='Arial')
 plt.tight_layout()
 plt.savefig('../Figures/fig_abstract_part1.pdf', bbox_inches='tight')
 plt.show()
@@ -54,14 +56,14 @@ fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharey=True)
 for i in range(3):
     axes[0].bar([i], [option_b_initial.loc[i, 'Oxford']], width=0.8, color=colors[i], yerr=option_b_initial.loc[i, 'Oxford SEM'], error_kw={'elinewidth':3})
 axes[0].set_xticks([0, 1, 2])
-axes[0].set_xticklabels(['Extreme', 'Severe', 'Mild'], fontsize=16)
+axes[0].set_xticklabels(['Extreme', 'Severe', 'Mild'], fontsize=16, fontname='Arial')
 for i in range(3):
     axes[1].bar([i], [option_b_recovery.loc[i, 'Oxford']], width=0.8, color=colors[i+3], yerr=option_b_initial.loc[i, 'Oxford SEM'], error_kw={'elinewidth':3})
 axes[1].set_xticks([0, 1, 2])
-axes[1].set_xticklabels(['Slow', 'Quick', 'Recovered'], fontsize=16)
+axes[1].set_xticklabels(['Slow', 'Quick', 'Recovered'], fontsize=16, fontname='Arial')
 axes[0].set_yticks([0, 10, 20, 30, 40, 50, 60])
-axes[0].set_yticklabels([0, 10, 20, 30, 40, 50, 60], fontsize=14)
-axes[0].set_ylabel('Stringency Index', fontsize=16)
+axes[0].set_yticklabels([0, 10, 20, 30, 40, 50, 60], fontsize=14, fontname='Arial')
+axes[0].set_ylabel('Stringency Index', fontsize=16, fontname='Arial')
 plt.tight_layout()
 plt.savefig('../Figures/fig_abstract_part2a.pdf')
 plt.show()
@@ -70,14 +72,14 @@ fig, axes = plt.subplots(1, 2, figsize=(8, 3), sharey=True)
 for i in range(3):
     axes[0].bar([i], [option_b_initial.loc[i, 'Mobility: Retail and Recreation']], width=0.8, color=colors[i], yerr=option_b_initial.loc[i, 'Mobility: Retail and Recreation SEM'], error_kw={'elinewidth':3})
 axes[0].set_xticks([0, 1, 2])
-axes[0].set_xticklabels(['Extreme', 'Severe', 'Mild'], fontsize=16)
+axes[0].set_xticklabels(['Extreme', 'Severe', 'Mild'], fontsize=16, fontname='Arial')
 for i in range(3):
     axes[1].bar([i], [option_b_recovery.loc[i, 'Mobility: Retail and Recreation']], width=0.8, color=colors[i+3], yerr=option_b_initial.loc[i, 'Mobility: Retail and Recreation SEM'], error_kw={'elinewidth':3})
 axes[1].set_xticks([0, 1, 2])
-axes[1].set_xticklabels(['Slow', 'Quick', 'Recovered'], fontsize=16)
+axes[1].set_xticklabels(['Slow', 'Quick', 'Recovered'], fontsize=16, fontname='Arial')
 axes[0].set_yticks([-60, -50, -40, -30, -20, -10, 0])
-axes[0].set_yticklabels([-60, -50, -40, -30, -20, -10, 0], fontsize=14)
-axes[0].set_ylabel('Mobility: Retail/Rec', fontsize=16)
+axes[0].set_yticklabels([-60, -50, -40, -30, -20, -10, 0], fontsize=14, fontname='Arial')
+axes[0].set_ylabel('Mobility: Retail/Rec', fontsize=16, fontname='Arial')
 plt.tight_layout()
 plt.savefig('../Figures/fig_abstract_part2b.pdf')
 plt.show()
